f1c80407a1db96dc2d51c49bf51ea96273f9cde6
Nick Benelli <31021342+nick-benelli@users.noreply.github.com>
2025-09-02 11:38:07 -0400

    Update run-package.sh

--- help/bash-commands/run-package.sh
+++ help/bash-commands/run-package.sh
@@ -1,1 +1,2 @@
-uv run -m ocean_report.main
+# uv run -m ocean_report.main # took out the __name__ == "__main__" in main.py
+uv run python -c "from ocean_report import run_report; run_report(run_email=False, test=True)"
